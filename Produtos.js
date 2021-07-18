ec0fad2fa3ca70aef03a45a92aa3dd0fc8ea60e3
Reginaldo4034 <86808073+Reginaldo4034@users.noreply.github.com>
2021-07-18 15:11:48 -0300

    Add files via upload

---
+++ Produtos.js
@@ -0,0 +1,11 @@
+let produtos = ["Carro", "Moto", "Geladeira"];
+console.log(produtos);
+let valores = [5000, 3000, 1500];
+console.log(valores[2])
+function  mostrarLista(produtos,valores){
+    return produtos;
+}
+mostrarLista(produtos);
+mostrarLista(valores);
+
+
---
+++ listaEmail.ts
@@ -0,0 +1,13 @@
+function mostrarEmail(email: string, nome:string) {
+    let nome = string("Reginaldo","Carlos","Mauro");
+    let email= string{["
+        regisantana30@hotmail.com",
+        "carlosjose42@gmail.com",
+        "mauroalmeidatrate@hotmail.com"]
+    };
+    let email: string = string[2];
+    console.log(email);
+}
+mostrarEmail(email, nome);
+
+
